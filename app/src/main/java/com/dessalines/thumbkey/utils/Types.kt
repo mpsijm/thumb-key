28ca389cdb9cd59aab06f3c6d4a629bb103d79cb
Maarten Sijm <9739541+mpsijm@users.noreply.github.com>
2024-04-19 16:50:32 +0200

    Types.kt: KeyC.display: also use `action.text` as default display text when action is ComposeLastKey

--- app/src/main/java/com/dessalines/thumbkey/utils/Types.kt
+++ app/src/main/java/com/dessalines/thumbkey/utils/Types.kt
@@ -55,6 +55,7 @@ data class KeyC(
     val display: KeyDisplay? =
         when (action) {
             is KeyAction.CommitText -> KeyDisplay.TextDisplay(action.text)
+            is KeyAction.ComposeLastKey -> KeyDisplay.TextDisplay(action.text)
             else -> null
         },
     val capsModeDisplay: KeyDisplay? = null,
